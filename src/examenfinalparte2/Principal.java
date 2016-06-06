690f49684e1f777e3f6bf76e18c3c56b7522b8e9
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-06 10:06:25 +0200

    Explicado el for que recorre el bucle

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -28,8 +28,53 @@ public class Principal {
         if(dig<=0)
         System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         
-        
+        /**
+         * Se crea un for para recorrer el bucle.
+         * @param aux es un parámetro que tenemos que crear, ya que si fuese i el resultado sería distinto
+         */
+        for(int i = 1; i <= 99999; i++ )
+        {
+            int aux = i;
  
+            int contador=0;
+ 
+            while (aux != 0){
+            aux = aux / 10;
+            contador++;
+        }
+        /**
+         * Se procede a realizar las operaciones del bucle.
+         * Aquí el bucle decirirá si mostrarlo luego o no.
+         * Si es igual a true se mostrará.
+         * Si es igual a false no se mostrará nada.
+         * Realizá las cuentas y finalmente las muestra
+         */
+            ndig=contador;
+            
+            
+            if(ndig==dig){
+                   if (i < 4) p = true;
+            else
+            {
+            if (i % 2 == 0) p = false; 
+                else
+                {
+                    int contador1 = 0;  
+                    int i1 = 1; 
+                    int k = (i - 1) / 2; 
+                    if  (k % 2 == 0) k--;  
+             
+                    while(i1 <= k)
+                    {
+                        if (i % i1 == 0) contador1++;
+                            i1 += 2;
+                            if (contador1 == 2) i1 = k + 1;
+                    }
+ 
+        if (contador1 == 1) p = true;
+            } 
+        } 
+     
        
  
     
