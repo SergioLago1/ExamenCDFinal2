669d4487563675d9fda160222fd7f81c03ba14dd
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2016-06-06 10:05:12 +0200

    Explicacion del programa y cambio para ingresar los num por teclado

--- src/examenfinalparte2/Principal.java
+++ src/examenfinalparte2/Principal.java
@@ -1,61 +1,36 @@
 package examenfinalparte2;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
-
+ /**
+     * Variable que en caso de ser true, indica que es un numero primo.
+     */
     public static boolean p = false;
-
-    public static void main(String arg[]) {
-        int dig = 2;
-        int ndig = 0;
-        if (dig <= 0) {
-            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        }
-        for (int i = 1; i <= 99999; i++) {
-            int aux = i;
-
-            int contador = 0;
-
-            while (aux != 0) {
-                aux = aux / 10;
-                contador++;
-            }
-            ndig = contador;
-
-            if (ndig == dig) {
-                if (i < 4) {
-                    p = true;
-                } else {
-                    if (i % 2 == 0) {
-                        p = false;
-                    } else {
-                        int contador1 = 0;
-                        int i1 = 1;
-                        int k = (i - 1) / 2;
-                        if (k % 2 == 0) {
-                            k--;
-                        }
-
-                        while (i1 <= k) {
-                            if (i % i1 == 0) {
-                                contador1++;
-                            }
-                            i1 += 2;
-                            if (contador1 == 2) {
-                                i1 = k + 1;
-                            }
-                        }
-
-                        if (contador1 == 1) {
-                            p = true;
-                        }
-                    }
-                }
-
-                if (p == true) {
-                    System.out.println(i);
-                }
-            }
-        }
-    }
-
+    /**
+     * Programa para visualizar los numeros primos con el numero de digitos que ha introducido el usuario
+     * @param arg 
+     */
+    public static void main(String arg[]){
+        /**
+         * Aqui pedimos por pantalla que ingresemos el numero de digitos que queremos que se nos muestre.
+         * @param ndig es igual a 0 para que no tome otro valor.
+         */
+        
+        int dig= Integer.parseInt(JOptionPane.showInputDialog("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): "));
+        int ndig=0;
+        
+        
+     /**
+      * Si el dígito es mayor que 0 se procede a funcionar el bucle, si se pone cero no iniciará el bucle.
+      */
+        
+        if(dig<=0)
+        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        
+        
+ 
+       
+ 
+    
 }
